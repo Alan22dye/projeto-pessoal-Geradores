a1a9fdb23c7f3b67909fe2761b5cf6dc5627502e
Alan Dyeison <159740301+Alan22dye@users.noreply.github.com>
2025-04-15 19:02:36 -0300

    Delete tests directory

--- tests/geradorSenhas.test.js
+++
@@ -1,8 +0,0 @@
-const gerarSenhaSegura = require('../src/geradorSenhas');
-
-test('gera senha segura com 12 caracteres e ao menos um caractere especial', () => {
-  const senha = gerarSenhaSegura();
-  
-  expect(senha).toHaveLength(12);
-  expect(senha).toMatch(/[!@#$%^&*(),.?":{}|<>]/);  // Verifica se tem caracteres especiais
-});
